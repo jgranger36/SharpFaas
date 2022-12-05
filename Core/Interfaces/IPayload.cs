68e088ecedab82a9a6a3b7fafc208a3e796469a9
jgranger <jgranger@yellowdogsoftware.com>
2022-12-05 06:24:20 -0500

    adding additional props to IPayload

--- Core/Interfaces/IPayload.cs
+++ Core/Interfaces/IPayload.cs
@@ -4,6 +4,8 @@ public interface IPayload
 {
     string Url { get; set; }
     Dictionary<string,string> Headers { get; set; }
+    public Guid InstanceId { get; set; }
+    public string CallerId { get; set; }
     Dictionary<string,string> Parameters { get; set; }
     string Body { get; set; }
 }
