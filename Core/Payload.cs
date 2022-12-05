ed6f5544283d24d35dfe6a321f508ad1475b1448
jgranger <jgranger@yellowdogsoftware.com>
2022-12-05 06:23:44 -0500

    add additional props to Payload

--- Core/Payload.cs
+++ Core/Payload.cs
@@ -6,6 +6,8 @@ namespace Core;
 public class Payload : IPayload
 {
     public string Url { get; set; }
+    public Guid InstanceId { get; set; }
+    public string CallerId { get; set; }
     public Dictionary<string, string> Headers { get; set; }
     public Dictionary<string, string> Parameters { get; set; }
     public string Body { get; set; }
