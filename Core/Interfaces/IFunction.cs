7854b341847f5db7f7ae94ff401c356036a74398
jgranger <jgranger@yellowdogsoftware.com>
2022-12-05 06:23:19 -0500

    clean up the IFunction interface to only include needed props

--- Core/Interfaces/IFunction.cs
+++ Core/Interfaces/IFunction.cs
@@ -3,10 +3,8 @@
 public interface IFunction 
 {
     Guid FunctionId { get; }
-    Guid InstanceId { get; }
     string Label { get; }
     string Description { get; }
-    DateTime StartTime { get; }
-    
+
     Task ExecuteAsync(IPayload payload);
 }
